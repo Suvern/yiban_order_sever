a19224547936eea96f36f91fa53838b71ead8d88
suvern <a821589498wmr>
2020-07-13 01:20:37 +0800

    解决跨域

--- yiban_order_server/asgi.py
+++ yiban_order_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for yiban_order_service project.
+ASGI config for yiban_order_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yiban_order_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yiban_order_server.settings')
 
 application = get_asgi_application()
